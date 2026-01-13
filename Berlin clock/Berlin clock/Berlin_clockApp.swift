05b14849b6e97b62788fbfe4bbfd5ede05b62f07
2026-DEV2-018 <254640193+2026-DEV2-018@users.noreply.github.com>
2026-01-13 16:47:34 +0100

    Initial commit

---
+++ Berlin clock/Berlin clock/Berlin_clockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Berlin_clockApp.swift
+//  Berlin clock
+//
+//  Created by 2026-DEV2-018 on 13/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct Berlin_clockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
