8e3efaf39444cdeedaae7173aea8aec5badeeaf3
2026-DEV2-018 <254640193+2026-DEV2-018@users.noreply.github.com>
2026-01-14 14:28:17 +0100

    Refactored the implementation for secondsLampIlluminated

--- Berlin clock/Berlin clock/BerlinClock.swift
+++ Berlin clock/Berlin clock/BerlinClock.swift
@@ -7,7 +7,7 @@
 
 struct BerlinClock {
     
-    func secondsLampIlluminated(seconds: Int) -> Bool {
-        return true
+    func secondsLampIluminated(seconds: Int) -> Bool {
+        seconds % 2 == 0
     }
 }
