ee582c737a66b43d879b7dda17b16b8819432572
2026-DEV2-018 <254640193+2026-DEV2-018@users.noreply.github.com>
2026-01-14 18:05:01 +0100

    Red updated test to filter on active lamps

--- Berlin clock/Berlin clockTests/Berlin_clockTests.swift
+++ Berlin clock/Berlin clockTests/Berlin_clockTests.swift
@@ -25,6 +25,7 @@ final class Berlin_clockTests: XCTestCase {
     func test_five_hours_row_returns_one_lamp_on_for_five_hours() {
         let clock = BerlinClock()
         let lamps = clock.fiveHoursRow(hours: 5)
+        let numberOfIlluminatedLamps = lamps.filter { $0.isOn }.count
         XCTAssertEqual(lamps.count, 1)
     }
 }
