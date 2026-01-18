e2d1a81480871077ddca1b39b95ee6847c27d07d
2026-DEV2-018 <254640193+2026-DEV2-018@users.noreply.github.com>
2026-01-18 13:20:30 +0100

    Green updated the value of oneMinuteLamps in the update method in the most simple way

--- Berlin clock/Berlin clock/BerlinClockViewModel.swift
+++ Berlin clock/Berlin clock/BerlinClockViewModel.swift
@@ -33,5 +33,6 @@ class BerlinClockViewModel {
         
         let minutes = calendar.component(.minute, from: date)
         fiveMinuteLamps = clock.fiveMinutesRow(minutes: minutes)
+        oneMinuteLamps = [.off, .off, .off, .off]
     }
 }
