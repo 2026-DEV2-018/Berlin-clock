43849e7be99b45489c7e4fd71774573b2171322c
2026-DEV2-018 <254640193+2026-DEV2-018@users.noreply.github.com>
2026-01-18 11:39:06 +0100

    Red call update method with date parameter

--- Berlin clock/Berlin clockTests/BerlinClockViewModelTests.swift
+++ Berlin clock/Berlin clockTests/BerlinClockViewModelTests.swift
@@ -13,6 +13,13 @@ final class BerlinClockViewModelTests: XCTestCase {
     func test_update_passes_correct_time_values_to_berlin_clock() async {
         let clock = await BerlinClock()
         let viewModel = await BerlinClockViewModel(clock: clock)
+        
+        let dateConponents = DateComponents(hour: 9, minute: 0, second: 0)
+        let date = Calendar.current.date(from: dateConponents)!
+        
+        viewModel.update(with: date)
+        
+        
     }
 
 }
